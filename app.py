48e7136d684af3d4c06d0e63195dff95b9f7178f
Alonso Silva <alonso.silva@gmail.com>
2024-04-23 09:28:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ class CounterWidget(anywidget.AnyWidget):
     _esm = """
         import confetti from "https://esm.sh/canvas-confetti@1.6.0"
         function render({ model, el }) {
-          let count = () => model.get("value");
+          let count = () => model.get("count");
           let btn = document.createElement("button");
           btn.innerHTML = `count is ${count()}`;
           btn.classList.add("counter-btn");
           btn.addEventListener("click", () => {
-            model.set("value", count() + 1);
+            model.set("count", count() + 1);
             model.save_changes();
           });
-          model.on("change:value", () => {
+          model.on("change:count", () => {
             btn.innerHTML = `count is ${count()}`;
             confetti({ angle: (45*(count()-1)) });
           });
@@ -32,12 +32,13 @@ class CounterWidget(anywidget.AnyWidget):
             background-color:green;
         }
     """
-    value = traitlets.Int(0).tag(sync=True)
+    count = traitlets.Int(0).tag(sync=True)
 
+vae = solara.reactive(0)
 @solara.component
 def Page():
     with solara.Column(style={"padding":"30px"}):
-        solara.Markdown("#Anywidgets+Solara")
+        solara.Markdown("#Anywidget+Solara")
         counter = CounterWidget()
         counter.element()
 Page()
