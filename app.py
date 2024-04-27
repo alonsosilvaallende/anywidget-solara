8d25a51a456192748e8d95e70945233748282f9c
Alonso Silva <alonso.silva@gmail.com>
2024-04-27 10:21:03 +0200

    Add slider

--- app.py
+++ app.py
@@ -5,40 +5,36 @@ import ipywidgets as widgets
 
 class CounterWidget(anywidget.AnyWidget):
     _esm = """
-        import confetti from "https://esm.sh/canvas-confetti@1.6.0"
-        function render({ model, el }) {
-          let count = () => model.get("count");
-          let btn = document.createElement("button");
-          btn.innerHTML = `count is ${count()}`;
-          btn.classList.add("counter-btn");
-          btn.addEventListener("click", () => {
-            model.set("count", count() + 1);
-            model.save_changes();
-          });
-          model.on("change:count", () => {
-            btn.innerHTML = `count is ${count()}`;
-            confetti({ angle: (45*(count()-1)) });
-          });
-          el.appendChild(btn);
-        }
-        export default { render };
+    import confetti from "https://esm.sh/canvas-confetti@1.6.0"
+    function render({ model, el }) {
+      let getCount = () => model.get("count");
+      let button = document.createElement("button");
+      button.classList.add("counter-button");
+      button.innerHTML = `count is ${getCount()}`;
+      button.addEventListener("click", () => {
+        model.set("count", getCount() + 1);
+        model.save_changes();
+      });
+      model.on("change:count", () => {
+        button.innerHTML = `count is ${getCount()}`;
+        confetti({ angle: getCount() });
+      });
+      el.appendChild(button);
+    }
+    export default { render };
     """
-    _css = """
-        .counter-btn {
-            background:blue;
-            padding:10px 50px;
-        }
-        .counter-btn:hover {
-            background-color:green;
-        }
+    _css="""
+    .counter-button { background:blue; padding:10px 50px;}
+    .counter-button:hover { background-color:green; }
     """
     count = traitlets.Int(0).tag(sync=True)
 
-vae = solara.reactive(0)
+counter = solara.reactive(0)
 @solara.component
 def Page():
     with solara.Column(style={"padding":"30px"}):
         solara.Markdown("#Anywidget+Solara")
-        counter = CounterWidget()
-        counter.element()
+        CounterWidget.element(count=counter.value, on_count=counter.set)
+        widgets.IntSlider.element(min=-180, max=180, value=counter.value, on_value=counter.set)
+        solara.Markdown(f"## Counter value is {counter.value}")
 Page()
