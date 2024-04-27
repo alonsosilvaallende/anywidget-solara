b0da30798c1227b053021ede476892057dccaa06
Alonso Silva <alonso.silva@gmail.com>
2024-04-27 11:23:39 +0200

    Add Github avatar

--- app.py
+++ app.py
@@ -29,12 +29,39 @@ class CounterWidget(anywidget.AnyWidget):
     """
     count = traitlets.Int(0).tag(sync=True)
 
+@solara.component
+def GithubAvatar(name: str, handle: str, img: str):
+    with solara.v.Html(tag="a", attributes={"href": f"https://twitter.com/{handle}/", "target": "_blank"}):
+        with solara.v.ListItem(class_="pa-0"):
+            with solara.v.ListItemAvatar(color="grey darken-3"):
+                solara.v.Img(
+                    class_="elevation-6",
+                    src=img,
+                )
+            with solara.v.ListItemContent():
+                solara.v.ListItemTitle(children=["Done with love by " + name])
+
 counter = solara.reactive(0)
 @solara.component
 def Page():
+    solara.lab.theme.themes.light.primary = "#0000ff"
+    solara.lab.theme.themes.dark.primary = "#0000ff"
+    with solara.AppBar():
+        solara.lab.ThemeToggle(enable_auto=False)
+    textcolor = "#000000" if solara.lab.use_dark_effective()==False else "#ffffff"
+    title = "Anywidget+Solara"
+    with solara.Head():
+        solara.Title(title)
     with solara.Column(style={"padding":"30px"}):
-        solara.Markdown("#Anywidget+Solara")
+        solara.Markdown(f"#{title}", style={"color": textcolor})
+        solara.Text("Click on the button or move the slider to activate a confetti animation.")
         CounterWidget.element(count=counter.value, on_count=counter.set)
         widgets.IntSlider.element(min=-180, max=180, value=counter.value, on_value=counter.set)
-        solara.Markdown(f"## Counter value is {counter.value}")
+        solara.Markdown(f"## Counter value is {counter.value}", style={"color": textcolor})
+        GithubAvatar(
+            "Alonso Silva",
+            "alonsosilva",
+            "https://avatars.githubusercontent.com/u/30263736?v=4",
+        )
+
 Page()
